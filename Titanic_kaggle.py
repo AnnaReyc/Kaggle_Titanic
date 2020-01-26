e7fc06eea648ee2abd1d1ce8957b30f42666b8e3
Анна <31421683+AnnaReyc@users.noreply.github.com>
2020-01-26 22:01:11 +0300

    2sd version

--- Titanic_kaggle.py
+++ Titanic_kaggle.py
@@ -5,12 +5,16 @@ dataset = pd.read_csv(url_train)
 #dataset.info() #видим, что типов object несколько: для обучения не нужны Name
 dataset.loc[dataset['Sex'] == 'male', 'Sex'] = 1
 dataset.loc[dataset['Sex'] == 'female', 'Sex'] = 2 #преобразуем для поля Sex 1-male 2-female
-dataset.loc[dataset['Embarked'] == 'S'] = 1
-dataset.loc[dataset['Embarked'] == 'C'] = 2
-dataset.loc[dataset['Embarked'] == 'Q'] = 3
+dataset.loc[dataset['Embarked'] == 'S', 'Embarked'] = 1
+dataset.loc[dataset['Embarked'] == 'C', 'Embarked'] = 2
+dataset.loc[dataset['Embarked'] == 'Q', 'Embarked'] = 3
 #dataset.fillna(0) #заменить все значения integer NaN на 0
-#print(dataset[dataset['Embarked'].isnull()]) #ищем значения float NaN
+#print(dataset[dataset['Age'].isnull()]) #ищем значения float NaN
 dataset['Embarked'] = pd.to_numeric(dataset['Embarked'], errors='coerce')
 dataset = dataset.dropna(subset=['Embarked'])
-dataset['Embarked'].astype(int)
-dataset.info()
+dataset['Age'] = pd.to_numeric(dataset['Age'], errors='coerce')
+dataset = dataset.dropna(subset=['Age'])
+#dataset['Embarked'].astype(int)
+dataset = dataset.drop(columns=['Cabin', 'Name'])#удаляем столбцы с object (axis=1 for columns, axis=0 for rows
+
+print(dataset)
